5157a4a1b4de5b842840ab097a3ba2aae7695932
Orlov Nikita <nikitosina2388@gmail.com>
2024-05-08 11:44:07 +0300

    2

---
+++ Zavod/DBclass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+namespace Zavod
+{
+    internal class DBclass
+    {
+        
+        public void Dbconn()
+        {
+            
+        }
+    }
+}
--- Zavod/Form1.cs
+++ Zavod/Form1.cs
@@ -1,10 +1,56 @@
+using System.Data.Entity;
+using System.Data.SQLite;
+
 namespace Zavod
 {
     public partial class Form1 : Form
     {
+        SQLiteConnection sqconn = new SQLiteConnection(@"Data Source=StorageDB.db");
+
+
+        DBclass db = new DBclass();
         public Form1()
         {
             InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReadData();
+        }
+
+        private void ReadData()
+        {
+            using (sqconn)
+            {
+                using (SQLiteCommand sqc = new SQLiteCommand("SELECT screw FROM Items")) 
+                {
+                    sqconn.Open();
+                    
+                    label7.Text = sqc.ToString();
+                    sqc.ExecuteNonQuery();
+                }
+                sqconn.Close();
+            }
+        }
+        
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ReadData();
         }
     }
 }
